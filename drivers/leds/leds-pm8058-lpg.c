d1137fddba15474dea77857111c335130786c973
Mark Boudreau <mboudreaudalton@gmail.com>
2020-07-08 16:59:09 -0400

    pm8058-lpg: Add error logging to get and set operations

--- drivers/leds/leds-pm8058-lpg.c
+++ drivers/leds/leds-pm8058-lpg.c
@@ -118,18 +118,26 @@ static void pm8058_lpg_set(struct led_classdev *cled,
 	enum led_brightness value)
 {
 	struct pm8058_lpg *lpg = container_of(cled, struct pm8058_lpg, cdev);
+	int ret = regmap_write(lpg->map, SSBI_REG_ADDR_LPG_CTL(3), value);
 
-	regmap_write(lpg->map, SSBI_REG_ADDR_LPG_CTL(3), value);
+	if (ret) {
+		pr_err("Failed to set LPG brightness\n");
+	}
 }
 
 static enum led_brightness pm8058_lpg_get(struct led_classdev *cled)
 {
 	struct pm8058_lpg *lpg = container_of(cled, struct pm8058_lpg, cdev);
-	enum led_brightness ret = 0;
+	enum led_brightness val = 0;
 
-	regmap_read(lpg->map, SSBI_REG_ADDR_LPG_CTL(3), &ret);
+	int ret = regmap_read(lpg->map, SSBI_REG_ADDR_LPG_CTL(3), &val);
 
-	return ret;
+	if (ret) {
+		pr_err("Failed to get LPG brightness\n");
+		return LED_OFF;
+	}
+
+	return val;
 }
 
 #if 0
